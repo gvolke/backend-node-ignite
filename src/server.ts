f2c4c4a2896b2b70519163a1ceb08100db5acf35
Gustavo Volke <gustavo_volke@hotmail.com>
2025-02-03 20:34:30 -0300

    arrumando deploy

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,6 @@ import { app } from "./app"
 import { env } from "./env"
 
 const port = env.PORT || 3337
-app.listen({ port: port }).then(() => console.log("HTTP server running"))
+app
+  .listen({ port: port, host: "0.0.0.0" })
+  .then(() => console.log("HTTP server running"))
